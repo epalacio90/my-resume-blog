8bde8ddefada2e3da84e58467d1508f17209e692
epalacio <ep@bookme.ai>
2022-06-14 16:25:15 -0500

    initial design

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', function(req, res, next) {
-    return res.status(200).json({success: 'Everything is working ok'});
+    return res.status(200).json({success: 'Everything is working ok :) !!'});
 });
 
 
